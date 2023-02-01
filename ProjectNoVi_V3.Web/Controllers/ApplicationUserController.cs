4fb03c21f4a1871f1e768b0ba28ea4d530afdba1
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2023-02-01 16:05:56 +0100

    Aplication View

--- ProjectNoVi_V3.Web/Controllers/ApplicationUserController.cs
+++ ProjectNoVi_V3.Web/Controllers/ApplicationUserController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectNoVi_V3.Models;
+using ProjectNoVi_V3.Web.ViewModels;
+using System.Collections.Generic;
 
 namespace ProjectNoVi_V3.Web.Controllers
 {
     public class ApplicationUserController : Controller
     {
-        public IActionResult Index()
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager) 
         {
-            return View();
+            _userManager = userManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users;
+            
+            List<ApplicationUserViewModel> list = new();
+
+            foreach (var user in users)
+            {
+                var role = await _userManager.GetRolesAsync(user);
+                list.Add(new ApplicationUserViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber,
+                    Role = role.FirstOrDefault(),
+                }); 
+            }
+
+            return View(list);
         }
     }
 }
---
+++ ProjectNoVi_V3.Web/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjectNoVi_V3.Enums;
+
+namespace ProjectNoVi_V3.Web.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Role { get; set; }
+    }
+}
